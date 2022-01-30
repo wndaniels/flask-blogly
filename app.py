fefcdaec83bb5bc88b6ad04d6c36e60dc8bfb6db
Nathan Daniels <williamnathandaniels@gmail.com>
2022-01-29 22:02:08 -0500

    Updated user files, added post files, and 404 page for redirection to home page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, current_app, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, Users
+from models import db, connect_db, Users, Post
 
 app = Flask(__name__)
 
@@ -23,17 +23,24 @@ def home_page():
     return redirect('/users')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """Show 404 NOT FOUND page."""
+
+    return render_template('/user/404.html'), 404
+
+
 @app.route('/users')
 def users_page():
     """ Show all users"""
     user_info = Users.query.all()
-    return render_template('user_list.html', user=user_info)
+    return render_template('/user/user_list.html', user=user_info)
 
 
 @app.route('/users/new')
 def new_user():
     """ Show form for adding new user """
-    return render_template('new_user.html')
+    return render_template('/user/new_user.html')
 
 
 @app.route('/users/new', methods=['POST'])
@@ -54,17 +61,17 @@ def create_new_user():
 def show_user(users_id):
     """ Show details of selected user """
     user = Users.query.get_or_404(users_id)
-    return render_template("user_details.html", user=user)
+    return render_template("/user/user_details.html", user=user)
 
 
-@app.route('/users/<int:users_id>/edit')
+@ app.route('/users/<int:users_id>/edit')
 def edit_user(users_id):
     """ Show form allowing User to edit their info """
-    user_info = Users.get_curr_by_id(users_id)
-    return render_template('edit_user.html', user_info=user_info)
+    user = Users.get_curr_by_id(users_id)
+    return render_template('/user/edit_user.html', user=user)
 
 
-@app.route('/users/<int:users_id>/edit', methods=['POST'])
+@ app.route('/users/<int:users_id>/edit', methods=['POST'])
 def edit_user_info(users_id):
     """ Edit users info, and update in list """
 
@@ -76,10 +83,10 @@ def edit_user_info(users_id):
     db.session.add(user)
     db.session.commit()
 
-    return redirect(f"/users/{user.id}")
+    return redirect(f"/users/{users_id}")
 
 
-@app.route('/users/<int:users_id>/delete', methods=["POST"])
+@ app.route('/users/<int:users_id>/delete', methods=["POST"])
 def delete_user(users_id):
     """ Delete users from list """
 
@@ -88,3 +95,69 @@ def delete_user(users_id):
     db.session.commit()
 
     return redirect('/users')
+
+
+@ app.route('/users/<int:users_id>/post-form')
+def user_post_form(users_id):
+    """ Show form allowing User to create a new post """
+
+    user = Users.get_curr_by_id(users_id)
+    return render_template('/post/new_post.html', user=user)
+
+
+@ app.route('/users/<int:users_id>/post-form', methods=['POST'])
+def user_post_new(users_id):
+    """ Show form allowing User to create a new post """
+
+    user = Users.query.get_or_404(users_id)
+    new_post = Post(title=request.form['title'],
+                    content=request.form['content'],
+                    users_id=users_id)
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(f"/users/{user.id}/post/{new_post.id}")
+
+
+@ app.route('/users/<int:users_id>/post/<int:post_id>')
+def user_post(users_id, post_id):
+    """ Show Users post details"""
+    user = Users.query.get_or_404(users_id)
+    post = Post.query.get_or_404(post_id)
+    return render_template('/post/post_details.html', user=user, post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def posts_edit(post_id):
+    """Show a form to edit an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('/post/edit_post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def posts_update(post_id):
+    """Handle form submission for updating an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+    flash(f"Post '{post.title}' edited.")
+
+    return redirect(f"/users/{post.users_id}/post/{post.id}")
+
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def posts_destroy(post_id):
+    """Handle form submission for deleting an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(f"/users/{post.users_id}")
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 """Models for Blogly."""
 
-from email.policy import default
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -17,24 +17,31 @@ DEFAULT_USER_IMAGE = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profi
 class Users(db.Model):
     __tablename__ = 'users'
 
-    def __repr__(self):
-        u = self
-        return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} image_url={u.image_url}>"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=True, default=DEFAULT_USER_IMAGE)
 
-    id = db.Column(db.Integer,
-                   primary_key=True,
-                   autoincrement=True)
-
-    first_name = db.Column(db.Text,
-                           nullable=False)
-
-    last_name = db.Column(db.Text,
-                          nullable=False)
-
-    image_url = db.Column(db.Text,
-                          nullable=True,
-                          default=DEFAULT_USER_IMAGE)
+    posts = db.relationship("Post", backref="users",
+                            cascade="all, delete-orphan")
 
     @classmethod
     def get_curr_by_id(cls, id):
+        return cls.query.filter_by(id=id).all()
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @classmethod
+    def get_curr_post_by_id(cls, id):
         return cls.query.filter_by(id=id).all()
